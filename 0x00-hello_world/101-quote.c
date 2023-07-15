27363924964fc3bba4950afd82a4c15d461972b4
GalaxyJ21 <Joseph>
2023-07-15 15:08:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
+ * main print an output without printf and puts
  *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
